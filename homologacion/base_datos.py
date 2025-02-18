79f90a21d46a7f9b09df865dd679e54f52340e28
pedrogil1919 <pedro.gil@uah.es>
2025-02-18 13:57:42 +0100

    añadir función para registrar un equipo.

--- homologacion/base_datos.py
+++ homologacion/base_datos.py
@@ -53,6 +53,31 @@ class Conexion():
         """
         return "%s (%s) - Usuario: %s" % (self.__database, self.__host, self.__user)
 
+    def registrar_equipo(self, fila, estado_actual):
+        """
+        Alterna el estado de registro de un equipo
+
+        Argumentos:
+        - fila: fila a la que corresponde el equipo. Tener en cuenta que el
+          número de fila no se corresponde con el dorsal, por lo que es
+          necesario determinar el dorsal a partir de la fila.
+
+        """
+        cursor_dorsal = self.__conexion.cursor(
+            dictionary=False, prepared=False)
+        cursor_equipo = self.__conexion.cursor(prepared=True)
+        cursor_dorsal.execute(
+            "SELECT ID_EQUIPO FROM ListaEquipos WHERE ORDEN = %s", (fila,))
+        if cursor_dorsal.rowcount != 1:
+            raise RuntimeError("Error en función de cambio de estado.")
+        dorsal = cursor_dorsal.fetchone()[0]
+        estado_nuevo = 1 if (estado_actual == estado.INSCRITO) else 0
+        cursor_equipo.execute(
+            "UPDATE Equipo SET registrado=%s WHERE ID_EQUIPO=%s", (estado_nuevo, dorsal))
+        if cursor_equipo.affected_rows != 1:
+            raise RuntimeError("Error en función de cambio de estado.")
+        self.__conexion.commit()
+
     def lista_equipos(self, estado):
         self.__conexion.rollback()
         cursor = self.__conexion.cursor(dictionary=False, prepared=False)
